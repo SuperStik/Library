90d5ba5d8ebcc78812c3b66ef515df4d76ea677d
Ryan Todd <rtodd@chapman.edu>
2023-03-15 15:00:49 -0700

    adds DVD.java

---
+++ DVD.java
@@ -0,0 +1,41 @@
+public Class DVD {
+    private String title;
+    private String genre;
+    private boolean isAvailable;
+
+    public DVD() {
+        title = "Spider-Man";
+        genre = "Action";
+        isAvailable = "True";
+    }
+
+    public DVD(String title, String genre, boolean isAvailable){
+        this.title = title;
+        this.genre = genre;
+        this.isAvailable = isAvailable;
+    }
+
+    public boolean()
+
+    // Accessors + Mutators
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setGenre(String genre){
+        this.genre = genre;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getGenre(){
+        return genre;
+    }
+
+    // toString Method
+
+
+
+}
