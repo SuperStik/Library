0e06a1b51180ad8e2555f6d4fedf98c9af19c853
SuperStik <daviswood290@gmail.com>
2023-03-15 15:01:10 -0700

    Add copy constructor

--- CardHolder.java
+++ CardHolder.java
@@ -11,5 +11,15 @@ public class CardHolder {
 		firstName = "unknown";
 		lastName = "unknown";
 		libraryCard = new LibraryCard();
+		numAudioBooks = 0;
+		numDVDs = 0;
+	}
+
+	CardHolder(CardHolder ch) {
+		firstName = ch.firstName;
+		lastName = ch.lastName;
+		libraryCard = new LibraryCard(ch.libraryCard);
+		numAudioBooks = ch.numAudioBooks;
+		numDVDs = ch.numDVDs;
 	}
 }
